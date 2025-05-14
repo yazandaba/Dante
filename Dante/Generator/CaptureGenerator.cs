d56679a0f159620d662a660d1130c48f263ac896
Yazan.Daba <yazandaba@outlook.com>
2025-05-15 01:17:45 +0400

    Add CaptureGenerator for C# nullable and control-flow-sensitive operation code generation

---
+++ Dante/Generator/CaptureGenerator.cs
@@ -0,0 +1,47 @@
+using Dante.Asserts;
+using Dante.Intrinsics;
+using Microsoft.CodeAnalysis.FlowAnalysis;
+using Microsoft.CodeAnalysis.Operations;
+using Microsoft.Z3;
+
+namespace Dante.Generator;
+
+/// <summary>
+///     this partition implements code generation of C# lowered nullable operations like null coalescing or null
+///     conditional access and other control flow sensetive operaionts,
+///     such operations are lowered in the control flow graph to be represented as null checking conditional basic block
+///     with operations of type <seealso cref="IFlowCaptureReferenceOperation" /> or
+///     <seealso cref="IFlowCaptureOperation" />
+/// </summary>
+internal partial class ExpressionGenerator
+{
+    public override Expr VisitIsNull(IIsNullOperation operation, GenerationContext argument)
+    {
+        return operation.Operand.Accept(this, argument)! is not DatatypeExpr expr
+            ? argument.SolverContext.MkFalse() //expression cannot be a maybe expression, so it cannot be null 
+            : MaybeIntrinsics.HasValue(expr);
+    }
+
+    public override Expr VisitFlowCapture(IFlowCaptureOperation operation, GenerationContext context)
+    {
+        var generatedValue = operation.Value.Accept(this, context) as Expr;
+        GenerationAsserts.RequireValidExpression(generatedValue, operation.Value);
+        _owningControlFlowGraphFlowCaptureTable.Bind(operation, generatedValue!);
+        //if flow capture operation value is a flow capture reference, then we should start building the graph
+        //to reach the captured intermediate value 
+        _owningControlFlowGraphFlowCaptureTable.TryTrack(operation);
+        return generatedValue!;
+    }
+
+    public override Expr VisitFlowCaptureReference(IFlowCaptureReferenceOperation operation, GenerationContext context)
+    {
+        //TODO we can return concrete value instead of Maybe when operation type is not nullable, but be careful such change may break other nullable related code generation
+        return _owningControlFlowGraphFlowCaptureTable.Fetch(operation);
+    }
+
+    public override MaybeExpr VisitDefaultValue(IDefaultValueOperation operation, GenerationContext argument)
+    {
+        var maybe = MaybeIntrinsics.CreateOrGet(operation.Type!);
+        return MaybeIntrinsics.Default(maybe);
+    }
+}
