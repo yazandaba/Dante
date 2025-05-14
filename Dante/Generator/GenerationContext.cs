8ada8c36110fcc012bc19bbd93f64dc92a5a1b94
yazan.daba <yazan@raincode.com>
2025-05-15 01:20:59 +0400

    Add `GenerationContext` and `FunctionGenerationContext` for managing code analysis and generation state

---
+++ Dante/Generator/GenerationContext.cs
@@ -0,0 +1,81 @@
+using Microsoft.CodeAnalysis.CSharp;
+using Microsoft.CodeAnalysis.FlowAnalysis;
+using Microsoft.Z3;
+
+namespace Dante.Generator;
+
+internal record GenerationContext
+{
+    private IntExpr? _recursionDepth;
+    private static GenerationContext? _generationContext;
+
+    protected GenerationContext()
+    {
+    }
+
+    public static GenerationContext CreateOrGet(Context solverContext, CSharpCompilation compilation,
+        uint recursionDepth)
+    {
+        _generationContext ??= new GenerationContext
+        {
+            SolverContext = solverContext,
+            SortPool = new SortPool(solverContext),
+            Compilation = compilation,
+            RecursionDepth = solverContext.MkInt(recursionDepth)
+        };
+
+        return _generationContext;
+    }
+
+    public static GenerationContext GetInstance()
+    {
+        return _generationContext ?? throw new InvalidOperationException(
+            $"getting instance of '{nameof(GenerationContext)}'" +
+            $"without creating it first. did you call CreateOrGet ?");
+    }
+
+    public required Context SolverContext { get; init; }
+    public required SortPool SortPool { get; init; }
+
+    /// <summary>
+    ///     the compilation unit of targeted C# file
+    /// </summary>
+    public required CSharpCompilation Compilation { get; init; }
+
+    /// <summary>
+    ///     class or struct members evaluation table
+    /// </summary>
+    public SymbolEvaluationTable GlobalEvaluationTable { get; } = new();
+
+    /// <summary>
+    ///     the virtual depth of recursion when abstract symbols are being used in loops
+    /// </summary>
+    /// <remarks>
+    ///     this will only get used in cases where the user didn't define that in code (e.g., user can explicitly set)
+    ///     the number of iterations in a for loop and the compiler will respect that, but if something like 'array.Length'
+    ///     was used, then that expression will get reduced to this
+    /// </remarks>
+    public IntExpr RecursionDepth
+    {
+        get => _recursionDepth ??= SolverContext.MkInt(1000);
+        init => _recursionDepth = value;
+    }
+}
+
+internal record FunctionGenerationContext : GenerationContext
+{
+    public required ControlFlowGraph TargetControlFlowGraph { get; init; }
+
+    public static FunctionGenerationContext Create(ControlFlowGraph targetControlFlowGraph)
+    {
+        var genContext = GetInstance();
+        return new FunctionGenerationContext
+        {
+            SolverContext = genContext.SolverContext,
+            SortPool = genContext.SortPool,
+            Compilation = genContext.Compilation,
+            TargetControlFlowGraph = targetControlFlowGraph,
+            RecursionDepth = genContext.RecursionDepth
+        };
+    }
+}
