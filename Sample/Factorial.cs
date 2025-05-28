456d44115a13c0ed3ba29eddbb73819fbc052a18
Yazan.Daba <yazandaba@outlook.com>
2025-05-28 21:05:51 +0400

    Add new sample 'Factorial' as a showcase of undecidable problem

---
+++ Sample/Factorial.cs
@@ -0,0 +1,23 @@
+﻿namespace Sample;
+
+public class Factorial
+{
+    public int Original(int x)
+    {
+        if (x <= 1) return 1;
+
+        return x * Original(x - 1);
+    }
+
+    public int Transformed(int x)
+    {
+        var factorial = 1;
+        while (x > 1)
+        {
+            factorial *= x;
+            --x;
+        }
+
+        return factorial;
+    }
+}
