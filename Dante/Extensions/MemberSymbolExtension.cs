9a16a4442aab0e6c485bad744e55edea0ad30d8a
yazan.daba <yazan@raincode.com>
2025-05-28 22:36:59 +0400

    Add 'GetMemberDeclaration' and 'GetDeclaration' to get in-source syntax declaration of the respective symbol
    (cherry picked from commit a8818a027fe24fbd144a584f87369efc94546d4e)

--- Dante/Extensions/MemberSymbolExtension.cs
+++ Dante/Extensions/MemberSymbolExtension.cs
@@ -1,4 +1,6 @@
 using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
 using Microsoft.CodeAnalysis.Operations;
 
 namespace Dante.Extensions;
@@ -7,12 +9,25 @@ internal static class MemberSymbolExtension
 {
     public static bool WasSourceDeclared(this ISymbol method)
     {
-        return method.DeclaringSyntaxReferences.Length is 0;
+        return method.DeclaringSyntaxReferences.Length > 0;
     }
 
     public static bool IsArrayLength(this IPropertyReferenceOperation property)
     {
-        return property.Instance?.Type is IArrayTypeSymbol { ElementType.SpecialType: SpecialType.System_Int32 } &&
+        return property.Instance?.Type is IArrayTypeSymbol
+               {
+                   ElementType.SpecialType: SpecialType.System_Int32 or SpecialType.System_Int64
+               } &&
                property.Property.Name is "Length";
+    }
+
+    private static CSharpSyntaxNode GetMemberDeclaration(ISymbol method)
+    {
+        return (CSharpSyntaxNode)method.DeclaringSyntaxReferences[0].GetSyntax();
+    }
+
+    public static MethodDeclarationSyntax GetDeclaration(this IMethodSymbol method)
+    {
+        return (MethodDeclarationSyntax)GetMemberDeclaration(method);
     }
 }
