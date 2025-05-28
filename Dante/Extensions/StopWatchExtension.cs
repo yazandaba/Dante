88ea4ae01ae823c259dc7baea4046639583979d8
Yazan.Daba <yazandaba@outlook.com>
2025-05-28 22:25:50 +0400

    Utility to properly display execution time
    (cherry picked from commit 2851fdb3e4ac3386f846d563f255f4014f7258a2)

---
+++ Dante/Extensions/StopWatchExtension.cs
@@ -0,0 +1,22 @@
+﻿using System.Diagnostics;
+
+namespace Dante.Extensions;
+
+internal static class StopWatchExtension
+{
+    public static void DisplayExecutionTime(this Stopwatch stopwatch, string operationName)
+    {
+        var elapsed = stopwatch.Elapsed;
+        Console.Write($"{operationName} completed in ");
+        Console.ForegroundColor = ConsoleColor.Green;
+        if (elapsed.TotalMilliseconds < 1)
+            Console.WriteLine($"{elapsed.TotalMicroseconds:F1} μs");
+        else if (elapsed.TotalSeconds < 1)
+            Console.WriteLine($"{elapsed.TotalMilliseconds:F2} ms");
+        else if (elapsed.TotalMinutes < 1)
+            Console.WriteLine($"{elapsed.TotalSeconds:F2} seconds");
+        else
+            Console.WriteLine($@"{elapsed:mm\:ss\.fff}");
+        Console.ResetColor();
+    }
+}
