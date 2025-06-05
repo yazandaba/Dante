cbbf8ffc48ec8bbf67ba4096c01f747d3f5a1e78
Yazan.Daba <yazandaba@outlook.com>
2025-06-05 20:46:58 +0400

    Add 'ComplexLoop' sample

--- Sample/Loop.cs
+++ Sample/Loop.cs
@@ -21,4 +21,46 @@ public class Loop
 
         return y;
     }
+
+    public int OriginalComplexLoop(int x, int y, int z, int c1, int c2)
+    {
+        x = x * 21 / y;
+        y += x * z;
+        while (c1 > 0 && c1 < c1 * 10)
+        {
+            y -= z;
+            while (c2 < 0 && c2 > c2 % 8 * -1)
+            {
+                z = y;
+                y = z + 1;
+                c2++;
+            }
+
+            c1 += 2;
+        }
+
+        return x;
+    }
+
+    public int TransformedComplexLoop(int x, int y, int z, int c1, int c2)
+    {
+        x = x * 21 / y;
+        y += x * z;
+        while (c1 > 0 && c1 < c1 * 10)
+        {
+            y -= z;
+            Back:
+            if (c2 < 0 && c2 > ~(c2 & 0x07) + 1)
+            {
+                c2++;
+                z = y;
+                y = z + 1;
+                goto Back;
+            }
+
+            c1 += 2;
+        }
+
+        return x;
+    }
 }
